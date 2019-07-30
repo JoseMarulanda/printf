e9d92047687adc41c04376c2e9bfb08b0c6ef9a1
JoseMarulanda <josejumarumol@gmail.com>
2019-07-30 22:17:29 +0000

    add decimal and integer function

--- functions.c
+++ functions.c
@@ -52,3 +52,49 @@ int print_percent(va_list args)
 	}
 	return (*str);
 }
+
+/**
+ * print_d - prints a decimal
+ * @args: decimal argument
+ * Return: counter
+ */
+int print_d(va_list args)
+{
+
+	unsigned int absolute, aux, countnum, count;
+	int n;
+
+	count = 0;
+	n = va_arg(args, int)
+		if (num < 0)
+		{
+			absolute = (n * -1);
+			count += _putchar('-');
+		}
+		else
+			anbsolute = n;
+
+	aux = absolute;
+	countnum = 1;
+	while (aux > 9)
+	{
+		aux /= 10;
+		countnum *= 10;
+	}
+	while (countnum >= 1)
+	{
+		count += _putchar(((absolute / countnum) % 10) + '0');
+		countnum /= 10;
+	}
+	return (count);
+}
+/**
+ * print_i - prints integer
+ * @args: integer argument
+ * Return: the decimal function
+ */
+
+int print_i(va_list args)
+{
+	return (print_d(args));
+}
