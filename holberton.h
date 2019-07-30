ffc70140253536e47f278c0e3a6d9b671f293c27
JoseMarulanda <josejumarumol@gmail.com>
2019-07-30 22:25:50 +0000

    add print int and print decimal function

--- holberton.h
+++ holberton.h
@@ -16,6 +16,8 @@ typedef struct specifier
 int _printf(const char *format, ...);
 int print_c(va_list args);
 int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
 int _putchar(char c);
 int print_percent(va_list args);
 int (*get_func(char x))(va_list args);
