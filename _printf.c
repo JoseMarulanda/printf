2ae407194abd84d4204707b4766467e791b9d65a
JoseMarulanda <josejumarumol@gmail.com>
2019-07-30 20:17:13 +0000

    fix null condition

--- _printf.c
+++ _printf.c
@@ -10,41 +10,43 @@
  */
 int _printf(const char *format, ...)
 {
-	if (format == NULL)
-		return (-1);
-	int count = 0, i;
-	int (*m)(va_list);
-	va_list args;
-
-	va_start(args, format);
-	i = 0;
-	if (format[0] == '%' && format[1] == '\0')
-		return (-1);
-	while (format != NULL && format[i] != '\0')
+	if (format != NULL)
 	{
-		if (format[i] == '%')
+		int count = 0, i;
+		int (*m)(va_list);
+		va_list args;
+
+		va_start(args, format);
+		i = 0;
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+		while (format != NULL && format[i] != '\0')
 		{
-			if (format[i + 1] == '%')
+			if (format[i] == '%')
 			{
-				count += _putchar(format[i]);
-				i += 2;
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i += 2;
+				}
+				else
+				{
+					m = get_func(format[i + 1]);
+					if (m)
+						count += m(args);
+					else
+						count = _putchar(format[i]) + _putchar(format[i + 1]);
+					i += 2;
+				}
 			}
 			else
 			{
-				m = get_func(format[i + 1]);
-				if (m)
-					count += m(args);
-				else
-					count = _putchar(format[i]) + _putchar(format[i + 1]);
-				i += 2;
+				count += _putchar(format[i]);
+				i++;
 			}
 		}
-		else
-		{
-			count += _putchar(format[i]);
-			i++;
-		}
+		va_end(args);
+		return (count);
 	}
-	va_end(args);
-	return (count);
+	return (-1);
 }
